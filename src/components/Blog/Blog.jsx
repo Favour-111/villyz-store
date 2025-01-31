20c7489744bdcf79009baaf45b96ed4f9e1283f5
favour <omojolaobaloluwa@gmail.com>
2025-01-31 13:36:40 +0100

    done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -14,27 +14,29 @@ const Blog = () => {
       <div className="content mt-1">
         we tackle intresting topics every day in {date}
       </div>
-      <div className="blog-container" data-aos="fade-up">
-        {blog.map((item) => {
-          return (
-            <div className="blog-item">
-              <div className="blog-image">
-                <img src={item.imageUrl} alt="" />
+      <div className="blog">
+        <div className="blog-container" data-aos="fade-up">
+          {blog.map((item) => {
+            return (
+              <div className="blog-item">
+                <div className="blog-image">
+                  <img src={item.imageUrl} alt="" />
+                </div>
+                <div className="date">{item.date}</div>
+                <div className="blog-content">{item.title}</div>
+                <button
+                  onClick={() => {
+                    window.scrollTo(0, 0);
+                    navigate("/SingleBlog");
+                  }}
+                  className="btn p-0 read-more"
+                >
+                  Read more
+                </button>
               </div>
-              <div className="date">{item.date}</div>
-              <div className="blog-content">{item.title}</div>
-              <button
-                onClick={() => {
-                  window.scrollTo(0, 0);
-                  navigate("/SingleBlog");
-                }}
-                className="btn p-0 read-more"
-              >
-                Read more
-              </button>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
