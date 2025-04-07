b7be10ef804be4ce7d25d38259054b16e27ae7e4
favour <omojolaobaloluwa@gmail.com>
2025-04-07 17:02:04 +0100

    done

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,10 @@ import "./Loading.css";
 const Loading = () => {
   return (
     <div className="loaderContainer">
-      <div className="loading">V</div>
+      <div>
+        <div className="loading">V</div>
+        <div className="loadingtext">Loading Page..</div>
+      </div>
     </div>
   );
 };
